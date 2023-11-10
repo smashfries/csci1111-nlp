afce59a7f2853329a0e21522a3eb19c8223e0b1c
Bilal <mejidk9919@gmail.com>
2023-11-10 15:50:41 -0500

    Updated Driver to read Files

--- Driver.java
+++ Driver.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        
+        try {
+      File myObj = new File("data/testdata.manual.2009.06.14.csv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-    
+  }
+        
+        
 }
+    
+
