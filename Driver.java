fcd1017972f216a19791415db5e130ad8af1f8f5
Aditya <52035504+smashfries@users.noreply.github.com>
2023-11-14 12:13:21 -0500

    create sentence object from csv lines and add to ArrayList

--- Driver.java
+++ Driver.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Driver {
         public static void main(String[] args) {
+                ArrayList<Sentence> sentences = new ArrayList<>();
                 try {
                         File myObj = new File("data/testdata.manual.2009.06.14.csv");
                         Scanner myReader = new Scanner(myObj);
                         while (myReader.hasNextLine()) {
                                 String data = myReader.nextLine();
+                                Sentence sentence = Sentence.convertLine(data);
+                                sentences.add(sentence);
                                 System.out.println(data);
                         }
                         myReader.close();
