e04acffb9399786e9e990f41f7db5c2e52565d0f
Bilal <mejidk9919@gmail.com>
2023-11-28 11:48:14 -0500

    Format, Hashmap

--- Driver.java
+++ Driver.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map; //place with imports
+import java.util.Collections; //place with imports
 
 public class Driver {
 
@@ -27,7 +30,7 @@ public class Driver {
 
                                 // prints out the line
                                 System.out.println(data);
-                                
+
                         }
                         myReader.close();
 
@@ -36,6 +39,47 @@ public class Driver {
                         System.out.println("An error occurred.");
                         error.printStackTrace();
                 }
+
+                HashMap<String, Integer> topwords = printTopWords(sentences);
+
+                Map.Entry<String, Integer> maxEntry = null;
+                for (Map.Entry<String, Integer> entry : topwords.entrySet())
+                        if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+                                maxEntry = entry;
+                int maxValueLen = maxEntry.getValue().toString().length();
+                ArrayList<String> results = new ArrayList<String>();
+                for (Map.Entry set : topwords.entrySet()) {
+                        String value = set.getValue().toString();
+                        while (value.length() < maxValueLen)
+                                value = " " + value;
+                        results.add(value + " of " + set.getKey());
+                }
+                Collections.sort(results);
+                Collections.reverse(results);
+                for (int i = 0; i < results.size() && i < 100; i++)
+                        System.out.println(results.get(i));
+
+        }
+
+        public static HashMap<String, Integer> printTopWords(ArrayList<Sentence> sentences) {
+
+                HashMap<String, Integer> hashMap = new HashMap<>();
+                for (int h = 0; h < sentences.size(); h++) {
+                        Sentence sentence = sentences.get(h);
+
+                        ArrayList<String> words = sentence.splitSentence();
+                        for (int b = 0; b < words.size(); b++) {
+                                if (hashMap.get(words.get(b)) != null) {
+                                        hashMap.put(words.get(b), hashMap.get(words.get(b)) + 1);
+
+                                } else {
+                                        hashMap.put(words.get(b), 1);
+                                }
+                        }
+
+                }
+                return hashMap;
+
         }
 
 }
--- Sentence.java
+++ Sentence.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+// Blueprint for Sentence object
 public class Sentence {
         private String text;
         private String author;
         private String timestamp;
 
+        // Constructor(Method) for Sentence passes in Text, Authort and timestamp
         public Sentence(String text, String author, String timestamp) {
                 this.text = text;
                 this.author = author;
@@ -13,10 +15,12 @@ public class Sentence {
 
         }
 
+        // gets text
         public String getText() {
                 return text;
         }
 
+        // sets text in private with the argument(Uses new string), does not return text
         public void setText(String text) {
                 this.text = text;
         }
@@ -39,31 +43,31 @@ public class Sentence {
         }
 
         public String toString() {
-                String reply = "{author:" + author + ", "  + "sentence:\"" + text + "\", " + "timestamp:\"" + timestamp + "\"}";
+                String reply = "{author:" + author + ", " + "sentence:\"" + text + "\", " + "timestamp:\"" + timestamp
+                                + "\"}";
                 return reply;
 
         }
 
         public static Sentence convertLine(String line) {
 
-                String[] convertLine = line.split("\","); 
+                String[] convertLine = line.split("\",");
                 for (int i = 0; i < convertLine.length; i++) {
                         convertLine[i] = convertLine[i].replaceAll("\\.|,|\"", "");
                 }
+                // datetimest stores the timestamp
                 String[] datetimest = convertLine[2].split(" ");
+                // comptime formats the timestamp as Month/Day/year
                 String comptime = datetimest[1] + " " + datetimest[2] + " " + datetimest[5];
-                                        
+
                 Sentence sentence = new Sentence(convertLine[5], convertLine[4], comptime);
-                
+
                 System.out.println(line);
                 System.out.println(convertLine.length);
                 System.out.println(Arrays.toString(convertLine));
 
-                
-
-
                 return sentence;
-                
+
         }
 
         public ArrayList<String> splitSentence() {
@@ -75,6 +79,5 @@ public class Sentence {
                 }
                 return arrlist;
         }
-
 
 }
